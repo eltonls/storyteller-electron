b2c8909459055c3063ca82349593b7e4d0b5687d
Elton <elton_dev@outlook.com>
2023-02-25 15:38:50 -0300

    chore: fixed some typescript bugs

--- packages/renderer/src/components/Writer/character.tsx
+++ packages/renderer/src/components/Writer/character.tsx
@@ -1,15 +1,17 @@
 import React from "react"
 import Writer from "./writer"
 
-const Character:React.FC = (props:any) => {
+function Character(props: { characterName: string }) {
   return (
     <div className="pl-5 h-full">
       <h2 className="font-bold text-xl text-purple-700">
         {props.characterName}
       </h2>
       <div className="flex flex-row">
-        <Writer compTitle="Características" />
-        <Writer compTitle="Corpo" />
+        <Writer compTitle="Fantasma" />
+        <Writer compTitle="Crença" />
+        <Writer compTitle="Necessidade" />
+        <Writer compTitle="Querer" />
       </div>
     </div>
   )
